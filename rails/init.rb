c2e20b219a59a6e638057b866c0e6da9882c96f6
Mani Tadayon <tmani@me.com>
2010-07-19 15:06:00 -0700

    Fix a bug with reloading of the behavior gem when config.cache_classes is set to false (usually in the development environment).
    Also make sure to check config is defined so that rspec can run correctly.

--- rails/init.rb
+++ rails/init.rb
@@ -1,1 +1,10 @@
 require 'behavior'
+
+# to prevent a bug when reloading when cache_classes=false (i.e. development environment)
+# see: http://strd6.com/2009/04/cant-dup-nilclass-maybe-try-unloadable/
+# and: http://github.com/paulca/behavior/issues#issue/1
+if defined?(config) and not config.cache_classes
+  load_paths.each do |path|
+    ActiveSupport::Dependencies.load_once_paths.delete(path)
+  end
+end
